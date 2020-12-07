d63a68e788fae59cbf02d11cfa4042a0fbabfae2
utkarshmega <utkarsheduc@gmail.com>
2020-12-07 09:01:21 +0530

    Uc1 to master

---
+++ src/test/java/com/capgemini/employeeparollappspring/EmployeePayrollApplicationTest.java
@@ -0,0 +1,14 @@
+package com.capgemini.employeeparollappspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmployeePayrollApplicationTest {
+	
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
